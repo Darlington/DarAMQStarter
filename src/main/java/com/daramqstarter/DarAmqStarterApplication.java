8876b363108789af34170e2f4e77ecf882a0b69b
Darlington <darlingto@hotmail.com>
2020-10-05 11:56:02 -0700

    initial commit

---
+++ src/main/java/com/daramqstarter/DarAmqStarterApplication.java
@@ -0,0 +1,13 @@
+package com.daramqstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarAmqStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DarAmqStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daramqstarter/DarAmqStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daramqstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DarAmqStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
